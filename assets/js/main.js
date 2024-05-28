9f843529cca01bd2c62d861701c482633c59a047
PawanC <pawan.c@marginframe.com>
2024-05-28 16:21:55 +0700

    custom theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
   const light = 'light';
   const dark = 'dark';
   let isDark = localStorage.theme === dark || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
-  if (isDark) {
+  if (false) {
     document.documentElement.classList.add(dark);
     themeToggle.checked = true;
   } else {
@@ -12,15 +12,15 @@
     themeToggle.checked = false;
   }
 
-  themeToggle.addEventListener('change', function () {
-    if (this.checked) {
-      localStorage.theme = dark;
-      document.documentElement.classList.add(dark);
-    } else {
-      localStorage.theme = light;
-      document.documentElement.classList.remove(dark);
-    }
-  });
+  // themeToggle.addEventListener('change', function () {
+  //   if (this.checked) {
+  //     localStorage.theme = dark;
+  //     document.documentElement.classList.add(dark);
+  //   } else {
+  //     localStorage.theme = light;
+  //     document.documentElement.classList.remove(dark);
+  //   }
+  // });
 
   const navbarMenuToggle = document.getElementById('navbar-menu-toggle');
   const navbarMenu = document.getElementById('navbar-menu');
